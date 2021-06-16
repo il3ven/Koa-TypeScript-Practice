d16e6beb2c559fc6e0191833b6ec2f30d925ea28
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2021-06-16 16:32:32 +0530

    added routes /hello, /echo, /error

--- Task 1/src/index.ts
+++ Task 1/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import Koa from "koa";
+import Router from "@koa/router";
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/hello", async (ctx) => {
+  ctx.response.body = "World";
+});
+
+router.get("/echo", async (ctx) => {
+  ctx.type = "application/json";
+  ctx.body = JSON.stringify(ctx.query);
+});
+
+router.get("/error", async (ctx) => {
+  ctx.throw(500, "Internal Server Error");
+});
+
+app.use(router.routes());
+
+app.listen(3000);
